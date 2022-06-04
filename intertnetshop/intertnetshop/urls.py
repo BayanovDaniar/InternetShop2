bc3eaee371f13f1789c869b189d44fb9d72e084f
Daniar Bayanov <daniar.2003@gmail.com>
2022-06-04 23:05:41 +0700

    Added models

--- intertnetshop/intertnetshop/urls.py
+++ intertnetshop/intertnetshop/urls.py
@@ -10,4 +10,9 @@ urlpatterns = [
     path('item', include('pageitem.urls')),
     path('reg', include('pageregistration.urls')),
     path('lovers', include('pagelovers.urls')),
+    path('test', include('mainapp.urls'))
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
