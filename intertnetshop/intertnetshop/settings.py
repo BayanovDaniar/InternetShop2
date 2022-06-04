b3bba7a035ccbea10d5682a83dd4c053bac89056
Daniar Bayanov <daniar.2003@gmail.com>
2022-06-04 15:39:42 +0700

    Fixed urls requests

--- intertnetshop/intertnetshop/settings.py
+++ intertnetshop/intertnetshop/settings.py
@@ -21,6 +21,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'pageregistration',
     'pageitem',
     'main',
     'django.contrib.admin',
--- intertnetshop/pageitem/views.py
+++ intertnetshop/pageitem/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def item(request):
+    return render(request, 'pageitem/index.html')
